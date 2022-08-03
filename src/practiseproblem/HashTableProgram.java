ff6d85c0eea578476392769b5086d2d3171c9b31
kasturi28 <kasturipansare2818@gmail.com>
2022-08-03 19:05:18 +0530

    Added_remove_avoidable_word

--- src/practiseproblem/HashTableProgram.java
+++ src/practiseproblem/HashTableProgram.java
@@ -5,53 +5,46 @@ import java.util.Map;
 
 public class HashTableProgram {
 
-	public static void main(String args[]) {
-
-		// Declaring the String
-		String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-
-		// Declaring a HashMap of <String, Integer>
-		Map<String, Integer> hashMap = new HashMap<>();
-
-		// Splitting the words of string
-		// and storing them in the array.
-		String[] words = str.split(" ");
-
-		for (String word : words) {
-
-			// Asking whether the HashMap contains the
-			// key or not. Will return null if not.
-			Integer integer = hashMap.get(word);
-
-			if (integer == null)
-				// Storing the word as key and its
-				// occurrence as value in the HashMap.
-				hashMap.put(word, 1);
-
-			else {
-				// Incrementing the value if the word
-				// is already present in the HashMap.
-				hashMap.put(word, integer + 1);
-			}
-		}
-		System.out.println(hashMap);
-
-		// code to remove whitespaces
-		String newStr = str.replaceAll(" ", "");
-
-		Map<Character, Integer> numCount = new HashMap<Character, Integer>(Math.min(newStr.length(), 26));
-
-		System.out.println("The count is: ");
-		for (int i = 0; i < newStr.length(); i++) {
-			char charAt = newStr.charAt(i);
-			if (!numCount.containsKey(charAt)) {
-				numCount.put(charAt, 1);
-			} else {
-				numCount.put(charAt, numCount.get(charAt) + 1);
-			}
-		}
-		System.out.println(numCount);
-
+		
+	    // Defining remove method
+	
+	    static void remove(String str, String word)
+	    {
+	        // Spliting the string using split() method
+	        String msg[] = str.split(" ");
+	        String new_str = "";
+	 
+	        // Iterating the string using for each loop
+	        for (String words : msg) {
+	 
+	            // If desired word is found
+	            if (!words.equals(word)) {
+	 
+	                // Concat the word not equal to the given
+	                // word
+	                new_str += words + " ";
+	            }
+	        }
+	 
+	        // Printing the original String & modified string
+	        
+	        System.out.println("Oringinal string : " +str);
+	        System.out.println("Modified string : " +new_str);
+	    }
+	 
+	    // Calling remove method to remove word avoidable
+	    
+	    public static void main(String[] args)
+	    {
+	        // Custom string as input
+	        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+	 
+	        // Word to be removed from above string
+	        String word = "avoidable";
+	 
+	        // Calling the method remove 
+	        remove(str, word);
+       
 	}
 
 }
